9b600d8b2e8f2f7222a57eb1ff36caa4cd1f3bd7
Hamza Abid <hamzaabid328@gmail.com>
2022-10-29 02:44:07 -0700

    Add new functions of array

--- ClassWork/29-10-2022/arrays.js
+++ ClassWork/29-10-2022/arrays.js
@@ -10,6 +10,8 @@ array = [1,2,3,4,5,6,7,8,9]
 
 // console.log(array['8'])
 
-let a=array.findIndex((ele)=>ele=='8')
-
-console.log(array[a]);
+let c=array.findIndex((ele)=>ele=='8')
+let a=array.find((ele)=>ele=='8')
+let b=array.filter((element)=>element=='1')
+console.log(a);
+console.log(b);
